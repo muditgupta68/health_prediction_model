aa030243ed1e83b0c0a7b6aa1d02b03f857fa83b
mudit gupta <muditgupta68@gmail.com>
2023-07-08 03:44:06 +0530

    minor updates

--- healthPred.py
+++ healthPred.py
@@ -3,6 +3,9 @@ import joblib
 
 
 def main():
+    url = 'https://colab.research.google.com/drive/18TNlAUxhtXS6DsyALau_Q1qVjoBHrlDw?usp=sharing'
+    st.markdown("🔗[Source Code](%s)" % url)
+
     html_temp = """
     <div style="background-color:lightblue;padding:16px">
     <h2 style="color:#232323;text-align:center">Health Insurance Cost Forecasting</h2>
@@ -11,8 +14,6 @@ def main():
     st.markdown(html_temp, unsafe_allow_html=True)
 
     model = joblib.load('health_model_gbr')
-    url = 'https://colab.research.google.com/drive/18TNlAUxhtXS6DsyALau_Q1qVjoBHrlDw?usp=sharing'
-    st.markdown("Source Code 🔗(%s)" % url)
     st.divider()
 
     age = st.slider('Enter your age', 18, 100)
@@ -21,51 +22,52 @@ def main():
     gender = st.selectbox(
         'Enter your Age',
         ('Male', 'Female'))
-    
+
     genderSwitcher = {
         "Male": 0,
-        "Female":1
-    }    
+        "Female": 1
+    }
     gender = genderSwitcher.get(gender)
     # st.write(gender)
-    
+
     bmi = st.number_input('Enter your BMI')
-    
-    children = st.slider('Enter Number of children',0,4)
-    
+
+    children = st.slider('Enter Number of children', 0, 4)
+
     smoke = st.selectbox(
         'Are you smoker',
         ('Yes', 'No'))
-    
+
     smokeSwitcher = {
-        'Yes':1,
-        'No' :0
+        'Yes': 1,
+        'No': 0
     }
-    
+
     smoke = smokeSwitcher.get(smoke)
     # st.write(smoke)
-    
+
     region = st.selectbox(
         'Region',
-        ('North-East', 'North-West','South-East','South-West'))
-    
-    selectedRegion = [0.0,0.0,0.0]
-    
+        ('North-East', 'North-West', 'South-East', 'South-West'))
+
+    selectedRegion = [0.0, 0.0, 0.0]
+
     if region == 'North-East':
-        selectedRegion = [0.0,0.0,0.0]
+        selectedRegion = [0.0, 0.0, 0.0]
     elif region == 'North-West':
-        selectedRegion = [1.0,0.0,0.0]
+        selectedRegion = [1.0, 0.0, 0.0]
     elif region == 'South-East':
-        selectedRegion = [0.0,1.0,0.0]
+        selectedRegion = [0.0, 1.0, 0.0]
     else:
-        selectedRegion = [0.0,0.0,1.0]
-    
+        selectedRegion = [0.0, 0.0, 1.0]
+
     # st.write(selectedRegion[0],selectedRegion[1],selectedRegion[2])
-    
+
     if st.button('Predict Now'):
-        prediction = model.predict([[age,gender,bmi,children,smoke,selectedRegion[0],selectedRegion[1],selectedRegion[2]]])
+        prediction = model.predict(
+            [[age, gender, bmi, children, smoke, selectedRegion[0], selectedRegion[1], selectedRegion[2]]])
         print(prediction[0])
-        
+
         st.success(f'Your Insurance Cost is {round(prediction[0],2)}')
 
 
